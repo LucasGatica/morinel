37fd5139111fab47d7474b43569313976f2a71e3
LucasGatica <lucas.gatica79@gmail.com>
2023-08-22 22:01:24 -0300

    testes

---
+++ scriptteste.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Formulário enviado com sucesso!');
+    // Aqui você pode adicionar o código para enviar os dados do formulário para um servidor ou banco de dados.
+});
