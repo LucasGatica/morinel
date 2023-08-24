0aeb9d0e2637271b53a74ef8c7ff2fb9ed627d5c
LucasGatica <30784336+LucasGatica@users.noreply.github.com>
2023-08-24 16:21:56 -0300

    tela de contato quase pronta

--- scriptteste.js
+++ scriptteste.js
@@ -1,5 +1,5 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Formulário enviado com sucesso!');
-    // Aqui você pode adicionar o código para enviar os dados do formulário para um servidor ou banco de dados.
-});
+function adjustInputWidth(inputElement) {
+    const spanElement = inputElement.previousElementSibling;
+    spanElement.textContent = inputElement.value;
+    inputElement.style.width = `${spanElement.offsetWidth}px`;
+}
